a7110caaf76854efc81f73c3b45fbc3b7568d3fa
Jamie Burke <jburke@padawan.lco.gtn>
2019-05-03 23:28:21 +0000

    Starting mysql backend branch for deployment on sn machine

--- snex2/settings.py
+++ snex2/settings.py
@@ -98,8 +98,8 @@ WSGI_APPLICATION = 'snex2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'snex2_db')
     }
 }
 
