699424d171ca01fdda501a3aba28dbaa708dc7b6
Lukasz Wyrzykowski <wyrzykow@visata.astrouw.edu.pl>
2019-10-12 00:28:33 +0200

    new run parameters for visataa

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python manage.py migrate --noinput
-gunicorn -b 0.0.0.0:8080 black_tom.wsgi
+gunicorn -b 0.0.0.0:8080 black_tom.wsgi --log-level debug --timeout 150 --workers 2 -k gevent
